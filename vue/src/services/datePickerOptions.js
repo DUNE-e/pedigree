a1caae42b8ef30bde793694d570ae2cf41632ea7
Daniil Tkachenko <danya.tk02@gmail.com>
2024-03-30 17:10:18 +0300

    tk_commit 6

--- vue/src/services/datePickerOptions.js
+++ vue/src/services/datePickerOptions.js
@@ -1,5 +1,7 @@
 export const parseDateString = (dateString) => {
-    if (!dateString) return null;
-    const [day, month, year] = dateString.split(".").map(Number);
-    return new Date(year, month - 1, day);
+    if (!dateString) {
+      return null
+    }
+    const [day, month, year] = dateString.split(".").map(Number)
+    return new Date(year, month - 1, day)
   }
