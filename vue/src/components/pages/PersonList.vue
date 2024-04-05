a214c460839a3e7155ff9fe2718b5f8c4410d1f4
Daniil Tkachenko <danya.tk02@gmail.com>
2024-04-05 15:55:11 +0300

    tk_commit 2

--- vue/src/components/pages/PersonList.vue
+++ vue/src/components/pages/PersonList.vue
@@ -64,7 +64,7 @@ export default {
     ]),
     filterRemovedData: {
       get(){
-        return this.filterRemoved.toString()
+        this.filterRemoved.toString()
       },
       set(value){
         this.filterRemoved = value == true
